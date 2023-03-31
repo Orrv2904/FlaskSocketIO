d2e69177d872c75ce0fddd654cbd141014bd7929
Orrv2904 <Omarrv2904@gmail.com>
2023-03-31 11:11:09 -0600

    :construction:

--- application.py
+++ application.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 app = Flask(__name__)
 
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    socketio.run(app)
  
